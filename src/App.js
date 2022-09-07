2e53013e71ffaf1fa72f94604e96ed208b4e702a
fikrinazilul1690 <fikrinazilul@gmail.com>
2022-09-07 14:47:31 +0700

    create login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Login from "./Login";
 import Register from "./Register";
 
 function App() {
   return (
     <main className="App">
-      <Register />
+      <Login />
     </main>
   );
 }
