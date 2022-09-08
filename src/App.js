272da1dc70ddc0adb90d959ccd6991aff2d35f13
fikrinazilul1690 <fikrinazilul@gmail.com>
2022-09-08 10:27:52 +0700

    implement react routes and protected routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import Login from "./Login";
-import Register from "./Register";
+import Login from "./componenents/Login";
+import Register from "./componenents/Register";
+import Layout from "./componenents/Layout";
+import RequireAuth from "./componenents/RequireAuth";
+import { Routes, Route } from "react-router-dom";
+import Home from "./componenents/Home";
+import LinkPage from "./componenents/LinkPage";
 
 function App() {
   return (
-    <main className="App">
-      <Login />
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/*public routes */}
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="linkpage" element={<LinkPage />} />
+
+        <Route element={<RequireAuth />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
